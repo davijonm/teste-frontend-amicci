f7233adc1c9379148ce230a3383645ed13f48d08
Davi Mendes <davi26030@gmail.com>
2025-04-05 11:39:47 -0300

    feat: add getWeatherByCity, a service to fetch data from open weather

---
+++ src/services/weatherService.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+export async function getWeatherByCity(city: string) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: city,
+        appid: API_KEY,
+        units: "metric",
+        lang: "pt_br",
+      },
+    });
+
+    const data = response.data;
+
+    return {
+      cidade: data.name,
+      pais: data.sys.country,
+      temperatura: data.main.temp,
+      descricao: data.weather[0].description,
+      umidade: data.main.humidity,
+      vento: data.wind.speed,
+      icone: data.weather[0].icon,
+    };
+  } catch {
+    throw new Error("Cidade não encontrada ou erro na API");
+  }
+}
